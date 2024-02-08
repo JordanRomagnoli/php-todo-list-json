bd70b0d9b8bd0dc567f7a07f0c7079bde4b828cf
JordanRomagnoli <jordanromagnoli1999@gmail.com>
2024-02-08 14:29:08 +0100

    - FINITO bonus; - ADD condizione per aggiungere la classe se la chiave "status" è true;

--- BACKEND/task.php
+++ BACKEND/task.php
@@ -1,6 +1,11 @@
 <?php
     $stringFromDb = file_get_contents('db.json');
     
+    $tasks = json_decode($stringFromDb, true);
+
+
+   
+
     header("Access-Control-Allow-Origin: *");
     header('Content-Type: application/json');
     echo $stringFromDb;
